74826d4d2f90c6215fa3f5e475b749d1e352c48f
Igor.Koloski <koloski_igor@hotmail.com>
2021-09-22 15:45:57 +0200

    Add negative tests for operations

--- src/components/logic/__tests__/calculate.test.js
+++ src/components/logic/__tests__/calculate.test.js
@@ -29,9 +29,19 @@ describe('handle buttons', () => {
     expect(calculate(obj, obj.operation)).toMatchObject({ next: null, operation: '+', total: '3' });
   });
 
+  test('Adding', () => {
+    const obj = { total: 1, next: 2, operation: '+' };
+    expect(calculate(obj, obj.operation)).not.toMatchObject({ next: null, operation: '+', total: '10' });
+  });
+
   test('Subtracting', () => {
     const obj = { total: 1, next: 2, operation: '-' };
     expect(calculate(obj, obj.operation)).toMatchObject({ next: null, operation: '-', total: '-1' });
+  });
+
+  test('Subtracting', () => {
+    const obj = { total: 1, next: 2, operation: '-' };
+    expect(calculate(obj, obj.operation)).not.toMatchObject({ next: null, operation: '-', total: '0' });
   });
 
   test('Multiplication', () => {
@@ -39,8 +49,18 @@ describe('handle buttons', () => {
     expect(calculate(obj, obj.operation)).toMatchObject({ next: null, operation: 'x', total: '4' });
   });
 
+  test('Multiplication', () => {
+    const obj = { total: 2, next: 2, operation: 'x' };
+    expect(calculate(obj, obj.operation)).not.toMatchObject({ next: null, operation: 'x', total: '8' });
+  });
+
   test('Division', () => {
     const obj = { total: 2, next: 2, operation: '÷' };
     expect(calculate(obj, obj.operation)).toMatchObject({ next: null, operation: '÷', total: '1' });
+  });
+
+  test('Division', () => {
+    const obj = { total: 2, next: 2, operation: '÷' };
+    expect(calculate(obj, obj.operation)).not.toMatchObject({ next: null, operation: '÷', total: '0' });
   });
 });
