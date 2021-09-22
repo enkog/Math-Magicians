c220a4c5a33a3beccc10ef3e26803aca304d3286
enkog <nkirukaogu@gmail.com>
2021-09-22 13:57:44 +0100

    Add tests for subtraction and multiplication in operate.test.js

--- src/components/logic/__tests__/operate.test.js
+++ src/components/logic/__tests__/operate.test.js
@@ -1,13 +1,30 @@
 import operate from '../operate'
 
-describe('operations', () => {
-    test('handle addition operation', () => {
-        it('tests that 4 + 7 equals 11', () => {
-            expect(operate(4, 7, '+')).toEqual('11');
-        })
-
-        it('tests that adds 4 + 7 does not equal 11', () => {
-            expect(operate(4, 7, '+')).not.toEqual('15');
-        })      
+describe('handle operations', () => {
+    test('that 4 + 7 equals 11', () => {
+        expect(operate(4, 7, '+')).toEqual('11');
+    })
+    
+    test('that adds 4 + 7 does not equal 11', () => {
+        expect(operate(4, 7, '+')).not.toEqual('15');
+    })
+    
+    test('that 10 - 6 equals 4', () => {
+        expect(operate(10, 6, '-')).toEqual('4');
+    })
+    
+    test('that adds 5 - 7 does not equal 11', () => {
+        expect(operate(5, 7, '-')).not.toEqual('2');
+    })
+    
+    test('that 2 x 3 equals 6', () => {
+        expect(operate(2, 3, 'x')).toEqual('6');
+    })
+    
+    test('tests that 4 x 7 does not equal 12', () => {
+        expect(operate(4, 7, 'x')).not.toEqual('12');
     })
 })
+      
+
+
