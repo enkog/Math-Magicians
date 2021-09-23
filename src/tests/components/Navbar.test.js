9ff33baea399258b3a1a72c465fb80b25fd8bff8
Igor.Koloski <koloski_igor@hotmail.com>
2021-09-23 12:34:38 +0200

    Add requested changes for the navbar tests

--- src/tests/components/Navbar.test.js
+++ src/tests/components/Navbar.test.js
@@ -3,6 +3,7 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from '../../components/UI/Navbar';
+import App from '../../App';
 
 it('should render the navbar', () => {
   const component = render(<Router><Navbar /></Router>);
@@ -10,24 +11,22 @@ it('should render the navbar', () => {
 });
 
 describe('Click events', () => {
-  test('that user to redirected to the Home page on clicking Home', () => {
-    render(<Router><Navbar /></Router>);
-    expect(screen.queryByText(/Welcome/)).toBeNull();
-    userEvent.click(screen.getByText('Home'));
+  test('expect home to be default page when the app starts', () => {
+    render(<Router><App /></Router>);
     expect(screen.queryByText(/Welcome/)).toBeInTheDocument();
   });
 
   test('that user to redirected to the Quote page on clicking Quote', () => {
-    render(<Router><Navbar /></Router>);
+    render(<Router><App /></Router>);
     expect(screen.queryByText(/Mathematics is not about numbers/)).toBeNull();
     userEvent.click(screen.getByText('Quote'));
     expect(screen.queryByText(/Mathematics is not about numbers/)).toBeInTheDocument();
   });
 
   test('that user to redirected to the Calculator page on clicking Calculator', () => {
-    render(<Router><Navbar /></Router>);
+    render(<Router><App /></Router>);
     expect(screen.queryByText('Lets do Some Math:')).toBeNull();
     userEvent.click(screen.getByText('Calculator'));
-    expect(screen.queryByText('Lets do Some Math:')).toBeInTheDocument();
+    expect(screen.queryByText("Let's do some math!")).toBeInTheDocument();
   });
 });
